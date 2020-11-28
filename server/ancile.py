9c0f6e47a4c7d952e9e440d637e94989c20ebed5
Mohamad Safadieh <4352580+msafadieh@users.noreply.github.com>
2020-11-27 22:50:54 -0500

    add write option

--- server/ancile.py
+++ server/ancile.py
@@ -11,7 +11,7 @@ def execute_program(program, dpps, status_queue, rpc_queuservere):
             app_module=None,
             rpc_queue=rpc_queue)
     status_queue.put_nowait(res)
-    with open("/data/result-{time()}.json") as f:
+    with open("/data/result-{time()}.json", "w") as f:
         json.dump(res, f)
     print("Killing thread....")
 
