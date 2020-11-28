d59adeb8eaf18f040421da687e765ddd4a01352d
Mohamad Safadieh <4352580+msafadieh@users.noreply.github.com>
2020-11-27 23:10:14 -0500

    Update ancile.py

--- server/ancile.py
+++ server/ancile.py
@@ -11,7 +11,7 @@ def execute_program(program, dpps, status_queue, rpc_queue):
             app_module=None,
             rpc_queue=rpc_queue)
     status_queue.put_nowait(res)
-    with open("/data/result-{time()}.json", "w") as f:
+    with open(f"/data/result-{time()}.json", "w") as f:
         json.dump(res, f)
     print("Killing thread....")
 
